4058c8a7a8ca4401787ea438a9635fe9bc84ee1d
Megane <87269645+MisakaMikoto10@users.noreply.github.com>
2021-07-25 21:45:55 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -2,6 +2,7 @@ let handler = function (m) {
   //ini hanyalah contoh, jangan di uncomment_-
   //Example this.sendContact(m.chat, '6281515860089', 'Nurutomo', m)
   this.sendContact(m.chat, '62887436568613', 'U n x', m)
+  this.sendContact(m.chat, '6285780372455', 'Megane',m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
