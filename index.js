c780c821ae8dae36131c184f3fac47240b928959
unx21 <84166927+unx21@users.noreply.github.com>
2021-07-26 12:31:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const CFonts  = require('cfonts')
 CFonts.say('Lightweight\nWhatsApp Bot', {
   font: 'chrome',
   align: 'center',
-  gradient: ['red', 'magenta']
+  gradient: ['blue', 'magenta']
 })
 CFonts.say(`'${package.name}' By @${package.author.name || package.author}`, {
   font: 'console',
   align: 'center',
-  gradient: ['red', 'magenta']
+  gradient: ['blue', 'magenta']
 })
 
 var isRunning = false
@@ -27,7 +27,7 @@ function start(file) {
   CFonts.say([process.argv[0], ...args].join(' '), {
     font: 'console',
     align: 'center',
-    gradient: ['red', 'magenta']
+    gradient: ['blue', 'magenta']
   })
   let p = spawn(process.argv[0], args, {
     stdio: ['inherit', 'inherit', 'inherit', 'ipc']
